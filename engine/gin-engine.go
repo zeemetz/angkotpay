464cba257879066a33ceaa0194af155fc69718f1
Yoedi <zeemetz@gmail.com>
2018-03-31 17:32:01 +0700

    add options header

--- engine/gin-engine.go
+++ engine/gin-engine.go
@@ -4,8 +4,28 @@ import "github.com/gin-gonic/gin"
 
 var engine *gin.Engine
 
+//CORSMiddleware  is
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			// fmt.Println("options")
+			c.JSON(200, nil)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func init() {
 	engine = gin.Default()
+	engine.Use(CORSMiddleware())
 }
 
 //GetAdmin is
